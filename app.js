522fc5c1ad2bd604e85e5e0eca26501442bc0133
Sakhavat Fleydanov <sexavetfv@gmail.com>
2026-02-03 23:04:32 +0400

    Snake Game V1 completed

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ const canvasSize = 600;
 const snakeSize = 20;
 let vX = snakeSize;
 let vY = 0;
+let score = 0;
+let apple = {
+  x: 160,
+  y: 160,
+};
 let snake = [
   {
-    x: 30,
-    y: 10,
+    x: 40,
+    y: 20,
   },
   {
     x: 20,
-    y: 10,
+    y: 20,
   },
   {
-    x: 10,
-    y: 10,
+    x: 0,
+    y: 20,
   },
 ];
 canvas.width = canvasSize;
@@ -31,6 +36,25 @@ drawSnake = () => {
     ctx.fillRect(block.x, block.y, snakeSize, snakeSize);
   });
 };
+drawApple = () => {
+  ctx.fillStyle = "#d63031";
+  ctx.fillRect(apple.x, apple.y, snakeSize, snakeSize);
+};
+drawScore = () => {
+  ctx.fillStyle = "#fff";
+  ctx.font = "30px Arial";
+  ctx.fillText(`Score: ${score}`, 460, 50);
+};
+generateApple = () => {
+  let rX = Math.round((Math.random() * (canvasSize - 20)) / 20) * 20;
+  let rY = Math.round((Math.random() * (canvasSize - 20)) / 20) * 20;
+  while (snake.find((a) => a.x === rX && a.y === rY)) {
+    rX = Math.round((Math.random() * (canvasSize - 20)) / 20) * 20;
+    rY = Math.round((Math.random() * (canvasSize - 20)) / 20) * 20;
+  }
+  apple.x = rX;
+  apple.y = rY;
+};
 moveSnake = () => {
   const head = { x: snake[0].x + vX, y: snake[0].y + vY };
   if (head.x > canvasSize) {
@@ -45,15 +69,54 @@ moveSnake = () => {
   if (head.y < 0) {
     head.y = canvasSize;
   }
+  if (head.x === apple.x && head.y === apple.y) {
+    snake.push(apple);
+    score++;
+    generateApple();
+  }
   snake.pop();
   snake.unshift(head);
-//   console.log(snake);
+  //   console.log(snake);
 };
 
-// setInterval(() => {
-//   drawBoard();
-//   moveSnake();
-//   drawSnake();
-// }, 1000);
+setInterval(() => {
+  drawBoard();
+  moveSnake();
+  drawSnake();
+  drawApple();
+  drawScore();
+}, 100);
+window.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 87:
+      if (vY > 0) {
+        break;
+      }
+      vX = 0;
+      vY = -snakeSize;
+      break;
+    case 68:
+      if (vX < 0) {
+        break;
+      }
+      vX = snakeSize;
+      vY = 0;
+      break;
+    case 83:
+      if (vY < 0) {
+        break;
+      }
+      vX = 0;
+      vY = snakeSize;
+      break;
+    case 65:
+      if (vX > 0) {
+        break;
+      }
+      vX = -snakeSize;
+      vY = 0;
+      break;
+  }
+});
 
 //* Snake Game Ends Here
